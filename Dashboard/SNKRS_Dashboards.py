6a29977d66e9d0b228cb86834752fa93c2717ad8
L <lucas.chevalier@mail-esd.com>
2021-12-07 00:58:32 +0100

    premier test streamlit

--- Dashboard/SNKRS_Dashboards.py
+++ Dashboard/SNKRS_Dashboards.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
 st.title('Hello World')
+
+data = pd.read_csv('../Data/export.csv', sep=',', encoding='utf-8')
+
+data.columns
+test = data[['brand', 'stockX', 'retailPrice']].groupby(['brand']).agg('mean')
+# fig = px.bar(test, x='retailPrice', y='stockX', title="test")
+# fig.show()
+
+st.line_chart(test)
